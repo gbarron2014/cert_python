62724d8bcaa710e8e7369ea685bdebfead4a985d
gbarron2014 <gbarron@utng.edu.mx>
2022-06-14 10:38:21 -0500

    Se modifico 2.1.1.16 y se agrego 2.1.1.18

---
+++ basico/modulo2/lab21118.py
@@ -0,0 +1,7 @@
+'''
+    Autor: Gabriel Barron
+    Fecha: 14 Junio
+    Descripcion: Usar end, sep
+'''
+print("Fundamentos","Programación","en", sep="#*", end=" ")
+print("Python")
--- basico/modulo2/lab2116.py
+++ basico/modulo2/lab2116.py
@@ -1,4 +1,6 @@
-# Laboratorio 2.1.1.6
-print('Hola Mundo')
-print()
-print("Gabriel Barron")
+# Autor: Gabriel Barron
+# Fecha: 14 Junio 2022
+# Descripcion: Hola Mundo
+
+print("Hola mundo!!")
+print('Gabriel Barron')
